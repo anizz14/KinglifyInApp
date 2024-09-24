14de3cedc144d570e46a4fd5792a8c36011929af
Anish Shrestha <anish@anzteams.com>
2024-09-24 19:31:28 +0545

    Initial Commit

---
+++ KinglifyInApp/KinglifyInAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinglifyInAppApp.swift
+//  KinglifyInApp
+//
+//  Created by Anish Shrestha on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct KinglifyInAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
